ff8e5581b4bf744974a6355245ee9ffc2a1cbbe4
Gidmaster <k.syrovatsky@gmail.com>
2019-04-19 14:15:32 +0300

    changed way to get secret key

--- chatApp.py
+++ chatApp.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret'
+app.config['SECRET_KEY'] = os.environ['APP_SECRET_KEY']
 socketio = SocketIO(app)
 
 @app.route('/', methods=['GET', 'POST'])
